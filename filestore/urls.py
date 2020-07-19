a4827b30c7d6fe423c23e72c3ec344cf75b50cce
NavyaEV <evnavya@gmail.com>
2020-07-19 20:05:07 +0530

    Update urls.py

--- filestore/urls.py
+++ filestore/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from filestore import views 
 
 urlpatterns = [
-    path('v1/files/', views.FileList.as_view()),
+    path('v1/files', views.FileList.as_view()),
     path('v1/files/download', views.FileDownload.as_view()),
 ]
